4eb4640432fe1af95c68ac90adc3f9ca91c8f9e8
VladimirVincan <vladimirvincan@gmail.com>
2019-10-23 06:36:02 -0500

    some things added

--- esl/main.cpp
+++ esl/main.cpp
@@ -1,12 +1,21 @@
 #include <systemc>
-#include "vp.hpp"
+// #include "vp.hpp"
+#include "utils.hpp"
+#include "typedefs.hpp"
 
 using namespace sc_core;
 using namespace tlm;
 
 int sc_main(int argc, char* argv[])
 {
-  Vp vp("VP");
-	sc_start(500, SC_NS);
+  //Vp vp("VP");
+	//sc_start(500, SC_NS);
+  unsigned char c[4];
+  to_uchar (c, 3.2);
+  for (int i = 0; i<4; ++i)
+    cout << (int)c[i] << endl;
+  num_t numb = -0.5;
+  cout << numb.to_bin() << endl;
+
   return 0;
 }
--- esl/typedefs.hpp
+++ esl/typedefs.hpp
@@ -7,8 +7,8 @@
 
 #define W 32 // DATA_WIDTH
 #define I 16 // FIXED_POINT_WIDTH
-#define Q sc_dt::SC_RND // quantization methods: SC_RND_ZERO, SC_RND_MIN_INF, SC_RND_INF, SC_RND_CONV, SC_TRN, SC_TRN_ZERO
-#define O sc_dt::SC_SAT // overflow methods: SC_SAT_ZERO, SC_SAT_SYM, SC_WRAP, SC_WRAP_SYM
+#define Q sc_dt::SC_TRN // quantization methods: SC_RND_ZERO, SC_RND_MIN_INF, SC_RND_INF, SC_RND_CONV, SC_TRN, SC_TRN_ZERO
+#define O sc_dt::SC_WRAP // overflow methods: SC_SAT_ZERO, SC_SAT_SYM, SC_WRAP, SC_WRAP_SYM
 typedef sc_dt::sc_fixed_fast <W, I, Q, O> num_t;
 
 typedef tlm::tlm_base_protocol_types::tlm_payload_type pl_t;
